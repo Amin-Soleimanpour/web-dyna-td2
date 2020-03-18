23002e0c5cfc491301d31e844a95e14b0669e7dd
Amin Soleimanpour <62330128+Bigmane@users.noreply.github.com>
2020-03-18 10:54:07 +0100

    Create test.php
    Ajout du fichier de test

---
+++ td1/test.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<?php
+	$version=phpversion();
+	echo "Version de php utilisee: $version"
+		?>
+</body>
+</html>

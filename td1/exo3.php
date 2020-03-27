18df849fbaee123d3851d2dbe930343e0f8d27d1
Amin Soleimanpour <62330128+Bigmane@users.noreply.github.com>
2020-03-27 10:32:58 +0100

    Created exo 3.php
    replace get with post

---
+++ td1/exo3.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>EX2</title>
+
+</head>
+<body>
+	<a href="?message=Rouge&size=12&couleur=red">Red</a>
+	<a href="?message=Vert&size=29&couleur=green">Green</a>
+	<a href="?message=Bleu&size=100&couleur=blue">Blue</a>
+
+<?php
+$size=$_POST['size']; 
+$color=$_POST['couleur'];
+$message=$_POST['message'];
+$update=$_POST['update'];
+
+
+if ($update == "+") {
+$size=$size+2;
+}
+if ($update == "-") {
+$size=$size-2;
+}
+
+if (isset($size) OR isset($color) OR isset($message))
+{  
+	if (isset($size) AND isset($color)) {
+	 echo "<div style='font-size: {$size}px; color: {$color} ;'>Message de taille {$size}px en {$message}</div>";
+}
+  else
+  {
+  	$color=Black;
+  	$size=12;
+  	echo "<div style='font-size: {$size}px; color: {$color} ;'>Message de taille {$size}px en Noir</div>";
+  }
+}
+else
+{ 
+	echo "Error";
+}
+
+ ?>
+<form method="POST">
+    <label for="message">Message : </label>
+    <input type="text" value="" name="message" id="message">
+    <label for="size">Size : </label>
+    <input type="number" value="" name="size" id="size">
+    <label for="couleur">Color : </label>
+    <input type="text" value="" name="couleur" id="couleur">   
+    <input type="submit" value="Valider">
+    <input type="number" name="size" id="size" value="<?=$size?>">
+    <input type="submit" name="update" value="+">
+    <input type="submit" name="update" value="-">
+</form>
+
+</body>
+</html>
